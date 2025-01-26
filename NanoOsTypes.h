43f433fd2ffc4263d0314ee8b27c18701da0c2da
James B. Card <jbcard@gmail.com>
2025-01-26 05:59:32 -0800

    Updating copyright dates in all the files.

--- NanoOsTypes.h
+++ NanoOsTypes.h
@@ -8,7 +8,7 @@
 /// @brief             Types used across NanoOs.
 ///
 /// @copyright
-///                   Copyright (c) 2012-2024 James Card
+///                   Copyright (c) 2012-2025 James Card
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a
 /// copy of this software and associated documentation files (the "Software"),
