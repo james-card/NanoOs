fa03bf42d6368a8b32c05271042f4dcaef4c6eec
James B. Card <jbcard@gmail.com>
2025-01-31 13:28:13 -0800

    Adding Rv32iVm library.

---
+++ Rv32iVm.h
@@ -0,0 +1,83 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// @author            James Card
+/// @date              01.31.2025
+///
+/// @file              Rv32iVm.h
+///
+/// @brief             Infrastructure for running RV32I-compiled programs in a
+///                    virtual machine within the OS.
+///
+/// @copyright
+///                   Copyright (c) 2012-2025 James Card
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+///                                James Card
+///                         http://www.jamescard.org
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef RV32I_H
+#define RV32I_H
+
+// Custom includes
+#include "NanoOs.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/// @struct Rv32iCoreState
+///
+/// @brief Structure to keep track of the state of a single virtual RV32I core.
+///
+/// @param x General-purpose registers (x0 - x31).
+/// @param pc The program counter.
+/// @param mstatus The machine status register.
+/// @param misa The machine ISA register.
+/// @param mie The machine interrupt enable.
+/// @param mtvec The machine trap vector.
+/// @param mscratch The machine scratch register.
+/// @param mepc The machine exception program counter.
+/// @param mcause The machine cause register.
+/// @param mtval The machine trap value.
+/// @param mip The machine interrupt pending.
+typedef struct Rv32iCoreState {
+  uint32_t x[32];
+  uint32_t pc;
+  uint32_t mstatus;
+  uint32_t misa;
+  uint32_t mie;
+  uint32_t mtvec;
+  uint32_t mscratch;
+  uint32_t mepc;
+  uint32_t mcause;
+  uint32_t mtval;
+  uint32_t mip;
+} Rv32iCoreState;
+
+int runRv32iProcess(int argc, char **argv);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // RV32I_H
