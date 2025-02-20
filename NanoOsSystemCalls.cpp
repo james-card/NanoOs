01bc1d32ceea86b0d314c0c8fe65d96bb45db96d
James B. Card <jbcard@gmail.com>
2025-02-20 15:39:17 -0800

    Restructuring NanoOsSystemCalls.cpp.

--- NanoOsSystemCalls.cpp
+++ NanoOsSystemCalls.cpp
@@ -1,6 +1,47 @@
 #include "NanoOsSystemCalls.h"
 #include "Rv32iVm.h"
 
+int32_t nanoOsSystemCallHandleExit(Rv32iVm *rv32iVm) {
+  // Get exit code from a0 (x10)
+  rv32iVm->running = false;
+  rv32iVm->exitCode = rv32iVm->rv32iCoreRegisters->x[10];
+  return 0;
+}
+
+int32_t nanoOsSystemCallHandleWrite(Rv32iVm *rv32iVm) {
+  // Get parameters from a0-a2 (x10-x12)
+  FILE *stream = (FILE*) rv32iVm->rv32iCoreRegisters->x[10];
+  uint32_t bufferAddress = rv32iVm->rv32iCoreRegisters->x[11];
+  uint32_t length = rv32iVm->rv32iCoreRegisters->x[12];
+  
+  // Limit maximum write length
+  if (length > NANO_OS_MAX_WRITE_LENGTH) {
+    length = NANO_OS_MAX_WRITE_LENGTH;
+  }
+  
+  // Read string from VM memory
+  char *buffer = (char*) malloc(length);
+  uint32_t bytesRead
+    = virtualMemoryRead(&rv32iVm->memorySegments[RV32I_DATA_MEMORY],
+    bufferAddress, length, buffer);
+  
+  // Write to the stream
+  fwrite(buffer, 1, bytesRead, stream);
+
+  // Free the host-side memory
+  free(buffer); buffer = NULL;
+  
+  // Return number of bytes written
+  rv32iVm->rv32iCoreRegisters->x[10] = bytesRead;
+  return 0;
+}
+
+typedef int32_t (*SystemCall)(Rv32iVm *rv32iVm);
+SystemCall systemCalls[] = {
+  nanoOsSystemCallHandleExit,  // NANO_OS_SYSCALL_EXIT
+  nanoOsSystemCallHandleWrite, // NANO_OS_SYSCALL_WRITE
+};
+
 /// @fn int32_t nanoOsSystemCallHandle(void *vm)
 ///
 /// @brief Handle NanoOs system calls from the running program.
@@ -14,45 +55,10 @@ int32_t nanoOsSystemCallHandle(void *vm) {
   // Get syscall number from a7 (x17)
   uint32_t syscallNumber = rv32iVm->rv32iCoreRegisters->x[17];
   
-  switch (syscallNumber) {
-    case NANO_OS_SYSCALL_WRITE: {
-      // Get parameters from a0-a2 (x10-x12)
-      FILE *stream = (FILE*) rv32iVm->rv32iCoreRegisters->x[10];
-      uint32_t bufferAddress = rv32iVm->rv32iCoreRegisters->x[11];
-      uint32_t length = rv32iVm->rv32iCoreRegisters->x[12];
-      
-      // Limit maximum write length
-      if (length > NANO_OS_MAX_WRITE_LENGTH) {
-        length = NANO_OS_MAX_WRITE_LENGTH;
-      }
-      
-      // Read string from VM memory
-      char *buffer = (char*) malloc(length);
-      uint32_t bytesRead
-        = virtualMemoryRead(&rv32iVm->memorySegments[RV32I_DATA_MEMORY],
-        bufferAddress, length, buffer);
-      
-      // Write to the stream
-      fwrite(buffer, 1, bytesRead, stream);
-
-      // Free the host-side memory
-      free(buffer); buffer = NULL;
-      
-      // Return number of bytes written
-      rv32iVm->rv32iCoreRegisters->x[10] = bytesRead;
-      return 0;
-    }
-    
-    case NANO_OS_SYSCALL_EXIT: {
-      // Get exit code from a0 (x10)
-      rv32iVm->running = false;
-      rv32iVm->exitCode = rv32iVm->rv32iCoreRegisters->x[10];
-      return 0;
-    }
-    
-    default: {
-      return -1;
-    }
+  if (syscallNumber < NUM_NANO_OS_SYSCALLS) {
+    return systemCalls[syscallNumber](rv32iVm);
   }
+  
+  return -1;
 }
 
