01fec3f140880ab7387a1bed7e9fb4a07ad6e776
James B. Card <jbcard@gmail.com>
2025-02-21 15:41:44 -0800

    Adding Doxygen content to NanoOsSystemCalls.cpp.

--- NanoOsSystemCalls.cpp
+++ NanoOsSystemCalls.cpp
@@ -1,6 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//                     Copyright (c) 2012-2025 James Card                     //
+//                                                                            //
+// Permission is hereby granted, free of charge, to any person obtaining a    //
+// copy of this software and associated documentation files (the "Software"), //
+// to deal in the Software without restriction, including without limitation  //
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,   //
+// and/or sell copies of the Software, and to permit persons to whom the      //
+// Software is furnished to do so, subject to the following conditions:       //
+//                                                                            //
+// The above copyright notice and this permission notice shall be included    //
+// in all copies or substantial portions of the Software.                     //
+//                                                                            //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR //
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   //
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    //
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER //
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    //
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        //
+// DEALINGS IN THE SOFTWARE.                                                  //
+//                                                                            //
+//                                 James Card                                 //
+//                          http://www.jamescard.org                          //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+// Doxygen marker
+/// @file
+
 #include "NanoOsSystemCalls.h"
 #include "Rv32iVm.h"
 
+/// @fn int32_t nanoOsSystemCallHandleExit(Rv32iVm *rv32iVm)
+///
+/// @brief Handle a user process exiting.
+///
+/// @param rv32iVm A pointer to the Rv32iVm object that's managing the program's
+/// state.
+///
+/// @return This function always returns 0.
 int32_t nanoOsSystemCallHandleExit(Rv32iVm *rv32iVm) {
   // Get exit code from a0 (x10)
   rv32iVm->running = false;
@@ -8,6 +46,14 @@ int32_t nanoOsSystemCallHandleExit(Rv32iVm *rv32iVm) {
   return 0;
 }
 
+/// @fn int32_t nanoOsSystemCallHandleWrite(Rv32iVm *rv32iVm)
+///
+/// @brief Handle a user process writing to a FILE pointer.
+///
+/// @param rv32iVm A pointer to the Rv32iVm object that's managing the program's
+/// state.
+///
+/// @return Returns 0 on success, negative error code on failure.
 int32_t nanoOsSystemCallHandleWrite(Rv32iVm *rv32iVm) {
   // Get parameters from a0-a2 (x10-x12)
   FILE *stream = (FILE*) rv32iVm->rv32iCoreRegisters->x[10];
@@ -36,7 +82,16 @@ int32_t nanoOsSystemCallHandleWrite(Rv32iVm *rv32iVm) {
   return 0;
 }
 
+/// @typedef SystemCall
+///
+/// @brief Definition for the structure of a system call handler in this
+/// library.
 typedef int32_t (*SystemCall)(Rv32iVm *rv32iVm);
+
+/// @var systemCalls
+///
+/// @brief Array of SystemCall functions that correpsond to the NanoOsSystemCall
+/// values that can be used by user processes.
 SystemCall systemCalls[] = {
   nanoOsSystemCallHandleExit,  // NANO_OS_SYSCALL_EXIT
   nanoOsSystemCallHandleWrite, // NANO_OS_SYSCALL_WRITE
