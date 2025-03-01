653f50943bc129a6df610ffb5ce67958823a2f8a
James B. Card <jbcard@gmail.com>
2025-02-28 17:48:53 -0800

    Initial commit of errno.h.

---
+++ usr/include/errno.h
@@ -0,0 +1,57 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// @author            James Card
+/// @date              02.20.2025
+///
+/// @file              errno.h
+///
+/// @brief             Implementation of errno.h for NanoOs C programs.
+///
+/// @copyright
+///                   Copyright (c) 2012-2025 James Card
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+///                                James Card
+///                         http://www.jamescard.org
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef ERRNO_H
+#define ERRNO_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef int errno_t;
+
+#define EDOM   1
+#define EILSEQ 2
+#define ERANGE 3
+
+extern int errno;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // ERRNO_H
+
