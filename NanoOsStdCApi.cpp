9515b19d734e19999033b12ce3ab1933966c47ee
James B. Card <jbcard@skymond-software.com>
2025-09-01 10:31:19 -0700

    Adding NanoOsStdCApi.cpp and NanoOsStdCApi.h.

---
+++ NanoOsStdCApi.cpp
@@ -0,0 +1,87 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//                     Copyright (c) 2012-2025 James Card                     //
+//                                                                            //
+// Permission is hereby granted, free of charge, to any person obtaining a    //
+// copy of this software and associated documentation files (the "Software"), //
+// to deal in the Software without restriction, including without limitation  //
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,   //
+// and/or sell copies of the Software, and to permit persons to whom the      //
+// Software is furnished to do so, subject to the following conditions:       //
+//                                                                            //
+// The above copyright notice and this permission notice shall be included    //
+// in all copies or substantial portions of the Software.                     //
+//                                                                            //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR //
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   //
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    //
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER //
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    //
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        //
+// DEALINGS IN THE SOFTWARE.                                                  //
+//                                                                            //
+//                                 James Card                                 //
+//                          http://www.jamescard.org                          //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+// Doxygen marker
+/// @file
+
+#include "NanoOsStdCApi.h"
+
+NanoOsStdCApi nanoOsStdCApi = {
+  // File operations:
+  .fopen = filesystemFOpen,
+  .fclose = filesystemFClose,
+  .remove = filesystemRemove,
+  .fseek = filesystemFSeek,
+  
+  // Formatted I/O:
+  .vsscanf = vsscanf,
+  .sscanf = sscanf,
+  .vfscanf = nanoOsVFScanf,
+  .fscanf = nanoOsFScanf,
+  .scanf = nanoOsScanf,
+  .vfprintf = nanoOsVFPrintf,
+  .fprintf = nanoOsFPrintf,
+  .printf = nanoOsPrintf,
+  .vsprintf = vsprintf,
+  .vsnprintf = vsnprintf,
+  .sprintf = sprintf,
+  .snprintf = snprintf,
+  
+  // Character I/O:
+  .fputs = nanoOsFPuts,
+  .puts = nanoOsPuts,
+  .fgets = nanoOsFGets,
+  
+  // Direct I/O:
+  .fread = filesystemFRead,
+  .fwrite = filesystemFWrite,
+  
+  // Memory management:
+  .free = memoryManagerFree,
+  .realloc = memoryManagerRealloc,
+  .malloc = memoryManagerMalloc,
+  .calloc = memoryManagerCalloc,
+  
+  // Copying functions:
+  .memcpy = memcpy,
+  .memmove = memmove,
+  .strcpy = strcpy,
+  .strncpy = strncpy,
+  .strcat = strcat,
+  .strncat = strncat,
+  
+  // Comparison functions:
+  .memcmp = memcmp,
+  .strcmp = strcmp,
+  .strncmp = strncmp,
+  
+  // Miscellaaneous string functions:
+  .memset = memset,
+  .strerror = nanoOsStrError,
+  .strlen = strlen,
+};
+
---
+++ NanoOsStdCApi.h
@@ -0,0 +1,109 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// @author            James Card
+/// @date              09.01.2025
+///
+/// @file              NanoOsStdCApi.h
+///
+/// @brief             Functionality from the standard C API that is to be
+///                    exported to user programs.
+///
+/// @copyright
+///                   Copyright (c) 2012-2025 James Card
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+///                                James Card
+///                         http://www.jamescard.org
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef NANO_OS_STD_C_API_H
+#define NANO_OS_STD_C_API_H
+
+#include "NanoOs.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct NanoOsStdCApi {
+  // File operations:
+  FILE* (*fopen)(const char *pathname, const char *mode);
+  int (*fclose)(FILE *stream);
+  int (*remove)(const char *pathname);
+  int (*fseek)(FILE *stream, long offset, int whence);
+  
+  // Formatted I/O:
+  int (*vsscanf)(const char *buffer, const char *format, va_list args);
+  int (*sscanf)(const char *buffer, const char *format, ...);
+  int (*vfscanf)(FILE *stream, const char *format, va_list ap);
+  int (*fscanf)(FILE *stream, const char *format, ...);
+  int (*scanf)(const char *format, ...);
+  int (*vfprintf)(FILE *stream, const char *format, va_list args);
+  int (*fprintf)(FILE *stream, const char *format, ...);
+  int (*printf)(const char *format, ...);
+  int (*vsprintf)(char *str, const char *format, va_list ap);
+  int (*vsnprintf)(char *str, size_t size, const char *format, va_list ap);
+  int (*sprintf)(char *str, const char *format, ...);
+  int (*snprintf)(char *str, size_t size, const char *format, ...);
+  
+  // Character I/O:
+  int (*fputs)(const char *s, FILE *stream);
+  int (*puts)(const char *s);
+  char* (*fgets)(char *buffer, int size, FILE *stream);
+  
+  // Direct I/O:
+  size_t (*fread)(void *ptr, size_t size, size_t nmemb, FILE *stream);
+  size_t (*fwrite)(const void *ptr, size_t size, size_t nmemb, FILE *stream);
+  
+  // Memory management:
+  void (*free)(void *ptr);
+  void* (*realloc)(void *ptr, size_t size);
+  void* (*malloc)(size_t size);
+  void* (*calloc)(size_t nmemb, size_t size);
+  
+  // Copying functions:
+  void* (*memcpy)(void *dest, const void *src, size_t n);
+  void* (*memmove)(void *dest, const void *src, size_t n);
+  char* (*strcpy)(char *dst, const char *src);
+  char* (*strncpy)(char *dst, const char *src, size_t dsize);
+  char* (*strcat)(char *dst, const char *src);
+  char* (*strncat)(char *dst, const char *src, size_t ssize);
+  
+  // Comparison functions:
+  int (*memcmp)(const void *s1, const void *s2, size_t n);
+  int (*strcmp)(const char *s1, const char *s2);
+  int (*strncmp)(const char *s1, const char *s2, size_t n);
+  
+  // Miscellaaneous string functions:
+  void* (*memset)(void *s, int c, size_t n);
+  char* (*strerror)(int errnum);
+  size_t (*strlen)(const char *s);
+} NanoOsStdCApi;
+
+extern NanoOsStdCApi nanoOsStdCApi;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // NANO_OS_STD_C_API_H
+
