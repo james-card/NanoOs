c08d16b0ff24e9835d4e9de2cb46f30cc19d5f5d
James B. Card <jbcard@skymond-software.com>
2025-09-02 18:10:44 -0700

    Removing automatically-generated content from usr/src/commands/helloworld/helloworld.c.

--- usr/src/commands/helloworld/helloworld.c
+++ usr/src/commands/helloworld/helloworld.c
@@ -9,26 +9,3 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-void* _start(void *args) {
-  MainArgs *mainArgs = (MainArgs*) args;
-  return (void*) ((intptr_t) main(mainArgs->argc, mainArgs->argv));
-}
-
-// These are the overlay functions exported.
-NanoOsOverlayExport exports[] = {
-  {"_start", _start},
-};
-
-// This needs to be the first thing in the overlay.
-__attribute__((section(".overlay_header")))
-NanoOsOverlayMap overlayMap = {
-  .header = {
-    .magic = NANO_OS_OVERLAY_MAGIC,
-    .version = (0 << 24) | (0 << 16) | (1 << 8) | (0 << 0),
-    .stdCApi = NULL,
-    .callOverlayFunction = NULL,
-    .numExports = sizeof(exports) / sizeof(exports[0]),
-  },
-  .exports = exports,
-};
-
