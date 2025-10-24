1a48f4cc49c5ce64ef1efa9a67b40a1e040c065a
James B. Card <jbcard@skymond-software.com>
2025-10-23 18:10:07 -0700

    Adding usr/include/unistd.h.

---
+++ usr/include/unistd.h
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////
+///
+/// @author            James Card
+/// @date              10.23.2025
+///
+/// @file              unistd.h
+///
+/// @brief             Functionality in the Unix unistd library.
+///
+/// @copyright
+///                   Copyright (c) 2012-2025 James Card
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a
+/// copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation
+/// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the
+/// Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included
+/// in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+///                                James Card
+///                         http://www.jamescard.org
+///
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef UNISTD_H
+#define UNISTD_H
+
+#include "NanoOsUser.h"
+
+#define gethostname(name, len) \
+  overlayMap.header.unixApi->gethostname(name, len)
+#define sethostname(name, len) \
+  overlayMap.header.unixApi->sethostname(name, len)
+
+#endif // UNISTD_H
+
