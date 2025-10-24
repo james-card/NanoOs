975096c777d4f2565478a80117b2ae29243c7698
James B. Card <jbcard@skymond-software.com>
2025-10-23 18:10:55 -0700

    Compiling version of getty.  Missing error logging due to missing errno implementation at this time.

--- usr/src/commands/getty/main.c
+++ usr/src/commands/getty/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
   (void) argc;
@@ -7,6 +9,13 @@ int main(int argc, char **argv) {
 
   char buffer[96];
   char *input = NULL;
+  char hostname[HOST_NAME_MAX + 1];
+  if (gethostname(hostname, HOST_NAME_MAX) != 0) {
+    //// fprintf(stderr, "gethostname failed with status: \"%s\"\n",
+    ////   strerror(errno));
+    strcpy(hostname, "localhost");
+  }
+  hostname[HOST_NAME_MAX] = '\0';
 
   while (input == NULL) {
     fputs("login: ", stdout);
