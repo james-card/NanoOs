5afc5c0caba82a51b977f182e15b8168d105f525
James B. Card <jbcard@skymond-software.com>
2025-10-24 18:49:28 -0700

    Filling out much more of the implementation for getty/main.c.

--- usr/src/commands/getty/main.c
+++ usr/src/commands/getty/main.c
@@ -34,25 +34,120 @@
 #include <unistd.h>
 #include <errno.h>
 
-int main(int argc, char **argv) {
-  (void) argc;
-  (void) argv;
-
-  char buffer[96];
-  char *input = NULL;
-  char hostname[HOST_NAME_MAX + 1];
+/// @fn int printHostname(void)
+///
+/// @brief Make the gethostname and return the hostname captured.
+///
+/// @return Returns 0 on success, -1 on failure.
+int printHostname(void) {
+  char *hostname = (char*) malloc(HOST_NAME_MAX + 1);
+  if (hostname == NULL) {
+    fprintf(stderr, "Unable to allocate memory for hostname.\n");
+    return -1;
+  }
+  
   if (gethostname(hostname, HOST_NAME_MAX) != 0) {
     fprintf(stderr, "gethostname failed with status: \"%s\"\n",
       strerror(errno));
-    strcpy(hostname, "localhost");
+    free(hostname);
+    return -1;
   }
   hostname[HOST_NAME_MAX] = '\0';
+  
+  fputs(hostname, stdout);
+  
+  free(hostname);
+  return 0;
+}
 
+/// @fn int printEscape(char escapeChar)
+///
+/// @brief Translate a getty escape character into the corresponding
+/// information and display it.
+///
+/// @param escapeChar The getty escape character.
+///
+/// @return Returns 0 on success, -1 on failure.
+int printEscape(char escapeChar) {
+  switch (escapeChar) {
+    case 'h': {
+      printHostname();
+      break;
+    }
+    
+    // We do *NOT* want a default case here.  If nothing matches, we need to
+    // silently ignore it.  Printing an error message would mangle the output.
+  }
+  
+  return 0;
+}
+
+/// @fn int showIssue(void)
+///
+/// @brief Display the contents of /etc/issue, replacing any escapes with the
+/// appropriate information.
+///
+/// @return Returns 0 on success, -1 on failure.
+int showIssue(void) {
+  char *buffer = (char*) malloc(96);
+  if (buffer == NULL) {
+    fputs("ERROR! Could not allocate space for buffer in showIssue.\n", stderr);
+    return -1;
+  }
+  
+  FILE *issueFile = fopen("/etc/issue", "r");
+  if (issueFile == NULL) {
+    fputs("ERROR! Could not open \"/etc/issue\" in showIssue.\n", stderr);
+    return -1;
+  }
+  
+  if (fgets(buffer, 96, issueFile) != buffer) {
+    fputs("ERROR! fgets did not read \"/etc/isue\"\n", stderr);
+  }
+  fclose(issueFile);
+  
+  char *nextPart = buffer;
+  char *backslashAt = strchr(nextPart, '\\');
+  while (backslashAt != NULL) {
+    *backslashAt = '\0';
+    fputs(nextPart, stdout);
+    
+    // Get to the escape character after the backslash.
+    nextPart = backslashAt + 1;
+    printEscape(*nextPart);
+    
+    // Skip over whatever the escape sequence was.
+    nextPart = &nextPart[strcspn(nextPart, " \t\n")];
+    backslashAt = strchr(nextPart, '\\');
+  }
+  fputs(nextPart, stdout);
+  
+  free(buffer);
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  (void) argc;
+  (void) argv;
+  
+  if (showIssue() != 0) {
+    // Error message, if any, was already displayed.
+    return 1;
+  }
+  
+  char *buffer = (char*) malloc(96);
+  if (buffer == NULL) {
+    fputs("ERROR! Could not allocate space for buffer in getty.\n", stderr);
+    return 1;
+  }
+  char *input = NULL;
+  
   while (input == NULL) {
     fputs("login: ", stdout);
     input = fgets(buffer, sizeof(buffer), stdin);
   }
-
+  
+  free(buffer);
   return 0;
 }
 
