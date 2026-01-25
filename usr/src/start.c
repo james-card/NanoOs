1ce0263c1e09ac0ff12a99b69a2be2352ec58efa
James B. Card <jbcard@gmail.com>
2026-01-24 18:27:43 -0800

    Adding 'environ' global variable to usr/src/start.c.

--- usr/src/start.c
+++ usr/src/start.c
@@ -2,8 +2,11 @@
 
 int main(int argc, char **argv);
 
+char **environ = NULL;
+
 void* _start(void *args) {
   MainArgs *mainArgs = (MainArgs*) args;
+  environ = overlayMap.header.env;
   return (void*) ((intptr_t) main(mainArgs->argc, mainArgs->argv));
 }
 
