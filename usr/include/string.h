82006d619aa294ed9e2074bafac7e03ac333631e
James B. Card <jbcard@gmail.com>
2026-01-25 13:58:56 -0800

    Converting function macros in usr/include/string.h to static inline functions.

--- usr/include/string.h
+++ usr/include/string.h
@@ -40,44 +40,61 @@
 #include "NanoOsUser.h"
 
 // Copying functions:
-#define memcpy(dest, src, n) \
-  overlayMap.header.osApi->memcpy(dest, src, n)
-#define memmove(dest, src, n) \
-  overlayMap.header.osApi->memmove(dest, src, n)
-#define strcpy(dest, src) \
-  overlayMap.header.osApi->strcpy(dest, src)
-#define strncpy(dest, src, dsize) \
-  overlayMap.header.osApi->strncpy(dest, src, dsize)
-#define strcat(dest, src) \
-  overlayMap.header.osApi->strcat(dest, src)
-#define strncat(dest, src, ssize) \
-  overlayMap.header.osApi->strncat(dest, src, ssize)
+static inline void *memcpy(void *dest, const void *src, size_t n) {
+  return overlayMap.header.osApi->memcpy(dest, src, n);
+}
+static inline void *memmove(void *dest, const void *src, size_t n) {
+  return overlayMap.header.osApi->memmove(dest, src, n);
+}
+static inline char *strcpy(char *dst, const char *src) {
+  return overlayMap.header.osApi->strcpy(dst, src);
+}
+static inline char *strncpy(char *dst, const char *src, size_t dsize) {
+  return overlayMap.header.osApi->strncpy(dst, src, dsize);
+}
+static inline char *strcat(char *restrict dst, const char *restrict src) {
+  return overlayMap.header.osApi->strcat(dst, src);
+}
+static inline char *strncat(char *dst, const char *src, size_t ssize) {
+  return overlayMap.header.osApi->strncat(dst, src, ssize);
+}
 
 // Search functions:
-#define memcmp(dest, src, n) \
-  overlayMap.header.osApi->memcmp(dest, src, n)
-#define strcmp(dest, src) \
-  overlayMap.header.osApi->strcmp(dest, src)
-#define strncmp(dest, src, n) \
-  overlayMap.header.osApi->strncmp(dest, src, n)
-#define strstr(haystack, needle) \
-  overlayMap.header.osApi->strstr(haystack, needle)
-#define strchr(s, c) \
-  overlayMap.header.osApi->strchr(s, c)
-#define strrchr(s, c) \
-  overlayMap.header.osApi->strrchr(s, c)
-#define strspn(s, accept) \
-  overlayMap.header.osApi->strspn(s, accept)
-#define strcspn(s, reject) \
-  overlayMap.header.osApi->strcspn(s, reject)
+static inline int memcmp(const void *s1, const void *s2, size_t n) {
+  return overlayMap.header.osApi->memcmp(s1, s2, n);
+}
+static inline int strcmp(const char *s1, const char *s2) {
+  return overlayMap.header.osApi->strcmp(s1, s2);
+}
+static inline int strncmp(const char *s1, const char *s2, size_t n) {
+  return overlayMap.header.osApi->strncmp(s1, s2, n);
+}
+static inline char *strstr(const char *haystack, const char *needle) {
+  return overlayMap.header.osApi->strstr(haystack, needle);
+}
+static inline char *strchr(const char *s, int c) {
+  return overlayMap.header.osApi->strchr(s, c);
+}
+static inline char *strrchr(const char *s, int c) {
+  return overlayMap.header.osApi->strrchr(s, c);
+}
+static inline size_t strspn(const char *s, const char *accept) {
+  return overlayMap.header.osApi->strspn(s, accept);
+}
+static inline size_t strcspn(const char *s, const char *reject) {
+  return overlayMap.header.osApi->strcspn(s, reject);
+}
 
 // Miscellaaneous string functions:
-#define memset(s, c, n) \
-  overlayMap.header.osApi->memset(s, c, n)
-#define strerror(errnum) \
-  overlayMap.header.osApi->strerror(errnum)
-#define strlen(s) \
-  overlayMap.header.osApi->strlen(s)
+static inline void *memset(void *s, int c, size_t n) {
+  return overlayMap.header.osApi->memset(s, c, n);
+}
+static inline char *strerror(int errnum) {
+  return overlayMap.header.osApi->strerror(errnum);
+}
+static inline size_t strlen(const char *s) {
+  return overlayMap.header.osApi->strlen(s);
+}
 
 #endif // STRING_H
 
